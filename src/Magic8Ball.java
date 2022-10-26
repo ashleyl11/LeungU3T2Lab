3296ea3ed1ccd5e2894d5aff37d01d8c1049fd44
student <sample@bths.edu>
2022-10-26 12:10:22 -0400

    creation

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -4,24 +4,60 @@ public class Magic8Ball
 {
     public static void main(String[] args)
     {
-        // Tell the user to enter a question
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the Magic 8 Ball!");
-        System.out.print("Enter a yes-no question and I will answer it: ");
-        String question = scan.nextLine();
+//        // Tell the user to enter a question
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Welcome to the Magic 8 Ball!");
+//        System.out.print("Enter a yes-no question and I will answer it: ");
+//        String question = scan.nextLine();
+//
+//        String response = "";  // update this variable with the response
+//
+//        // Write code here to generate a random number from 1 to 6
+//        int randomNum = (int)(Math.random()* (6 - 1 + 1)) + 1;
+//
+//        // Write code here to use
+//        // appropriate selection statements (if, if else, else)
+//        // to choose from 1 of 6 responses and set response to it.
+//        // for example, if the number is 1, set response to "Outlook good"
+//        // if it’s 2, set response to "Without a doubt!"
+//        // etc.
+//
+//        if (randomNum == 1){
+//            response += "Most definately!";
+//        }
+//        if (randomNum == 2){
+//            response += "There's a high chance!";
+//        }
+//        if (randomNum == 3){
+//            response += "Sure!";
+//        }
+//        if (randomNum == 4){
+//            response += "Maybe...";
+//        }
+//        if (randomNum == 5){
+//            response += "Probably not";
+//        }
+//        if (randomNum == 6){
+//            response += "Of course not!";
+//        }
+//
+//        System.out.println(response);  // print response
+        int score = 53;
+        String grade = "";  // initialize to empty string
 
-        String response = "";  // update this variable with the response
+        if (score >= 90) {
+            grade = "A";
+        } else if (score >= 80) {
+            grade = "B";
+        } else if (score >= 70) {
+            grade = "C";
+        } else if (score >= 60) {
+            grade = "D";
+        } else if (score < 60) {
+            grade = "F";
+        }
+        System.out.println("Your grade is: " + grade);
 
-        // Write code here to generate a random number from 1 to 6
-
-        // Write code here to use
-        // appropriate selection statements (if, if else, else)
-        // to choose from 1 of 6 responses and set response to it.
-        // for example, if the number is 1, set response to "Outlook good"
-        // if it’s 2, set response to "Without a doubt!"
-        // etc.
-
-        System.out.println(response);  // print response
     }
 }
 
--- src/SelectionMadness.java
+++ src/SelectionMadness.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class SelectionMadness {
+public class SelectionMadness
+{
+    // No instance variables //
+
+    // no-parameter constructor with "empty" body since no instance variables
+    public SelectionMadness() {}
+
+    /* Simulates randomly flipping a fair coin and returning true for heads and
+       false for tails; hint: this method should use the Math.random() method to
+       generate a random number.  This method returns true (heads) 50% of the
+       time and false (tails) 50% of the time, on average.
+     */
+
+
+
+
+    public boolean flipCoin()
+    { /* implement this method! */
+        int random = (int)((Math.random() * (10-1+1))+1);
+        if (random < 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /* Returns the largest of three provided integers: num1, num2, or num3
+     */
+    public int largest(int num1, int num2, int num3)
+    { /* implement this method! */
+        int compare;
+        if (num1 > num2){
+            compare = num1;
+        } else {
+            compare = num2;
+        }
+        if (num3 > compare){
+            compare = num3;
+        }
+        return compare;
+    }
+
+
+    /* Returns true if the three provided lengths create a right triangle, in
+       other words, a2 + b2 = c2, where c is the longest side; returns false
+       Otherwise (hint: use the largest() method that you wrote above!).
+       Be careful -- this is a bit more challenging than it seems!  Consider if
+       side1 = 3, side2 = 4, and side3 = 5 vs. if side1 = 5, side2 = 4, and
+       side3 = 3 -- will your code work regardless of which side is the longest
+       of the three?
+     */
+    public boolean rightTriangle(int side1, int side2, int side3)
+    { /* implement this method! */
+        int largest = largest(side1, side2, side3);
+        int a;
+        int b;
+
+        if (side1 == largest){
+            a = side2;
+            b = side3;
+        } else if (side2 == largest){
+            a = side1;
+            b = side3;
+        } else {
+            a = side1;
+            b = side2;
+        }
+
+        return Math.pow(a,2) + Math.pow(b,2) == Math.pow(largest,2);
+    }
+
 }
